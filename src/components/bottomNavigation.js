f2975ac191f692680c7d26cb79d76b8659139d1d
Joan Rueda <joanruedapau@gmail.com>
2020-05-07 13:54:38 +0200

    contact email

--- src/components/bottomNavigation.js
+++ src/components/bottomNavigation.js
@@ -6,6 +6,11 @@ import ListIcon from "@material-ui/icons/List";
 import MapIcon from "@material-ui/icons/Map";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import ContactSupportIcon from "@material-ui/icons/ContactSupport";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
 
 const useStyles = makeStyles({
   root: {
@@ -19,9 +24,23 @@ const useStyles = makeStyles({
   },
 });
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function SimpleBottomNavigation(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <BottomNavigation
@@ -51,11 +70,28 @@ export default function SimpleBottomNavigation(props) {
         className={classes.btns}
       />
       <BottomNavigationAction
+        onClick={handleClickOpen}
         label="contacta"
         icon={<ContactSupportIcon />}
         className={classes.btns}
-        href=" mailto:joanruedapau@gmail.com?subject=He%20trobat%20dades%20incorrectes"
       />
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {"Email de contacte"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            <a href=" mailto:joanruedapau@gmail.com">joanruedapau@gmail.com</a>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
     </BottomNavigation>
   );
 }
